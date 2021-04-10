9829e0ddaba9be2d41c1c0dbe10d11b117348fb3
Lucas Laet <lucas.laetlira@gmail.com>
2021-04-10 15:51:36 -0300

    Configurações de Seg. e CORS

---
+++ src/main/java/br/com/hinto/util/config/ConfigSeg.java
@@ -0,0 +1,61 @@
+package br.com.hinto.util.config;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class ConfigSeg extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private UserDetailsService servico;
+	
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.cors().and().csrf().disable();
+
+        httpSecurity.authorizeRequests().anyRequest().permitAll();
+
+        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource(){
+
+        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(),
+                HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+        
+        final UrlBasedCorsConfigurationSource urlConfiguration = new UrlBasedCorsConfigurationSource();
+        urlConfiguration.registerCorsConfiguration(
+                "/**", configuration);
+        
+        return urlConfiguration;
+    }
+    
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.servico).passwordEncoder(passwordEncoder());
+    }
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    	return new BCryptPasswordEncoder();
+    }	
+}
