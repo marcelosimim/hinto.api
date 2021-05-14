1a959e75b23a5900174305c3a2eb03bc5ad97768
Lucas Laet <lucas.laetlira@gmail.com>
2021-05-13 23:52:09 -0300

    Config de segurança CORS

--- src/main/java/br/com/hinto/util/config/ConfigSeg.java
+++ src/main/java/br/com/hinto/util/config/ConfigSeg.java
@@ -1,16 +1,12 @@
 package br.com.hinto.util.config;
 
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,23 +26,20 @@ public class ConfigSeg extends WebSecurityConfigurerAdapter {
 
         httpSecurity.cors().disable();
         httpSecurity.csrf().disable();
-        httpSecurity.headers().frameOptions().disable();
 
         httpSecurity.authorizeRequests().anyRequest().permitAll();
-
-        httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
     CorsConfigurationSource corsConfigurationSource(){
 
-        CorsConfiguration configuration = new CorsConfiguration().applyPermitDefaultValues();
-        configuration.setAllowedMethods(Arrays.asList(HttpMethod.GET.name(),
-                HttpMethod.POST.name(), HttpMethod.PUT.name(), HttpMethod.DELETE.name(), HttpMethod.OPTIONS.name()));
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
         
         final UrlBasedCorsConfigurationSource urlConfiguration = new UrlBasedCorsConfigurationSource();
-        urlConfiguration.registerCorsConfiguration(
-                "/**", configuration);
+        urlConfiguration.registerCorsConfiguration("/**", configuration);
         
         return urlConfiguration;
     }
